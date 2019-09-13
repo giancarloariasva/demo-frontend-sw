3a14c048e6ca1df2e957ec9ae048496d32ad3eb9
Giancarlo Arias Silva <giancarloariasva@gmail.com>
2019-09-14 00:51:59 +0200

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="" >
+      <div className="Starjedi bottom ">
+        altran
+        </div>
+        <div className="SerGothic sin-espacio">
+          {/* SHOW CASTINGS GAME */}
+          {/* LOGICAMENTE... BUSCANDO CURRO */}
+          {/* GIANCARLO Y MARIA EN UNA GALAXIA LEJANA */}
+          FELICITACIONES STEFANY!!!
+        </div>
+      <div className="Starjedi top ">
+        WARS
+      </div>
+    </div>
+  )
+}
+
+export default Home;
