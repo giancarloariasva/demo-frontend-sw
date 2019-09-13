9e6147bb301dbc1bc3f794635e80341797ad6ab6
Giancarlo Arias Silva <giancarloariasva@gmail.com>
2019-09-14 01:04:29 +0200

    Updates

---
+++ precache-manifest.fe6bef26f829787d1e872dd6e3a4bb34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e9aeed4d77e228f5c1e54628bf49b8",
+    "url": "/demo-frontend-sw/index.html"
+  },
+  {
+    "revision": "42c5dfdc7269d0b87936",
+    "url": "/demo-frontend-sw/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3cb1133ae5c5f01af041",
+    "url": "/demo-frontend-sw/static/css/main.3f91ff85.chunk.css"
+  },
+  {
+    "revision": "42c5dfdc7269d0b87936",
+    "url": "/demo-frontend-sw/static/js/2.1cc1a514.chunk.js"
+  },
+  {
+    "revision": "3cb1133ae5c5f01af041",
+    "url": "/demo-frontend-sw/static/js/main.0f4b7e28.chunk.js"
+  },
+  {
+    "revision": "1d6463b5f5c5881b4f8d",
+    "url": "/demo-frontend-sw/static/js/runtime~main.5a2d71ee.js"
+  },
+  {
+    "revision": "7585858f88aca2a77794bd1735563068",
+    "url": "/demo-frontend-sw/static/media/r2d2.7585858f.png"
+  }
+]);
